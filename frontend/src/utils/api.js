903f9a9964d7c77c7c07ef332f5fed0aff387901
leonardo.silva <leonardo.silva@integrativa.com.br>
2023-12-08 14:55:09 -0300

    alteração...

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -2,8 +2,8 @@ import axios from "axios"
 
 /*export default axios.create({
     baseURL: "http://localhost:5000/",
-})
+})*/
 
 export default axios.create({
     baseURL: "http://15.229.39.121:5000/",
-})*/
+})
